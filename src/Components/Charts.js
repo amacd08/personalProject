8e8466b7c9b8cd30eb4398617af9bf6b0cde583d
amacd08 <amacd08@gmail.com>
2019-06-22 18:40:58 -0600

    added line in charts js

--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -33,8 +33,7 @@ class Chart extends Component {
         let pars = []
         let under_pars = []
         let CourseLabels = []
-        for (let i = 0; i < roundList.length -1; i++){
-            console.log(roundList[i])
+        for (let i = 0; i <= roundList.length -1; i++){
             fairways.push(roundList[i].total_fairways)
             girs.push(roundList[i].total_gir)
             lostBalls.push(roundList[i].total_lostball)
